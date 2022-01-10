d2c49754e9a67ecb8721750dc9d382466ff7ea0d
DaniiellPC <daniel17alejandro@gmail.com>
2022-01-10 17:20:04 -0500

    feat: se agregan variables de entorno

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const puerto = 5000
+require('dotenv').config()
 const rutas = require('./routes')
 
+const puerto = process.env.PORT
 
 app.use(express.static('public'))
 
@@ -11,10 +12,11 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(rutas)
 
+app.set('port', puerto || 5000)
 app.set('views', path.join(__dirname, '../public/views'))
 app.set('view engine', 'ejs')
 
 
-app.listen(puerto, () => {
-    console.log(`Server on port ${puerto}`)
+app.listen(app.get('port'), () => {
+    console.log(`Server on port:`, app.get('port'))
 })
